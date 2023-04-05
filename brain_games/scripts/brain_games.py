3fabca3e30f2a27527705c5d32cd30d729626e9b
Nikolay Shved <viktor.vaskes44@gmail.com>
2023-04-05 16:39:11 +0800

    modified brain_games.py, __pycache__/brain_games.cpython-310.pyc, ../cli.py, ../__pycache__/cli.cpython-310.pyc

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.cli import welcome_user
 
-import prompt
+
+def main():
+    print('Welcome to the Brain games!')
+    print(welcome_user())
 
 
-def welcome_user():
-    print('Welcome to the Brain Games')
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    if name == 'main':
-       main(welcome_user)
+if __name__ == '__main__':
+    main()
