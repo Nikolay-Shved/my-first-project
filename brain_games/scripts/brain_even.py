13e1df2cd207ec14c83fdcfc7c6021d9d61279bc
Nikolay Shved <viktor.vaskes44@gmail.com>
2023-04-05 19:17:45 +0800

    modified brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -13,16 +13,18 @@ def main():
         num = random.randint(1, 100)
         print(f'Questions: {num}')
         if num % 2 == 0:
-            num = 'yes'
+            result = 'yes'
         else:
-            num = 'no'
-            answer = prompt.string('Your answer: ')
-        if answer == num:
+            result = 'no'
+
+        answer = prompt.string('Your answer: ')
+
+        if answer == result:
             print('Correct!')
             i += 1
 
         else:
-            print(f'{answer} is wrong answer ;(. Correct answer was {num}.')
+            print(f'{answer} is wrong answer ;(. Correct answer was {result}.')
             print(f"Let's try again, {name}!")
             break
 
