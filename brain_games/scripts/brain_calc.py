9bdbaeb2ddccaa2420cb0f64df13a7f68745b335
Nikolay Shved <viktor.vaskes44@gmail.com>
2023-04-06 16:33:18 +0800

    add brain_calc.py

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,41 @@
+import random
+import prompt
+from random import choice
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('What is the result of the expression?.')
+
+    i = 0
+    while i < 3:
+        operation = ['+', '-', '*']
+        num1 = random.randint(1, 100)
+        num2 = random.randint(1, 100)
+        num3 = choice(operation)
+        print(f'Question: {num1} {num3} {num2}')
+        if num3 == '+':
+            result = num1 + num2
+        elif num3 == '-':
+            result = num1 - num2
+        else:
+            result = num1 * num2
+
+        answer = prompt.string('Your answer: ')
+
+        if int(answer) == int(result):
+            print('Correct!')
+            i += 1
+        else:
+            print(f"{answer} is wrong answer ;(. Correct answer was {result}.")
+            print(f"Let's try again, {name}!")
+            break
+
+    if i == 3:
+        print(f'Congratulations, {name}!')
+
+
+if __name__ == '__main__':
+    main()
