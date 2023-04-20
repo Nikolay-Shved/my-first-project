43f449b3fe1853b4ad86e8ea2ce8981dffa3a2ae
Nikolay Shved <viktor.vaskes44@gmail.com>
2023-04-20 13:15:44 +0800

    modified brain_prime, logic, brain_progression

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -11,7 +11,7 @@ def prime_game(name):
         print(f'Question: {num}')
 
         k = 0
-        for i in range(2, num // 2+1):
+        for i in range(2, num // 2 + 1):
             if (num % i == 0):
                 k += 1
         if (k <= 0):
