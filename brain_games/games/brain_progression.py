64f099afca4c24b90b3c7bc10d5dedb24469ec2d
Nikolay Shved <viktor.vaskes44@gmail.com>
2023-05-06 16:41:21 +0800

    modified brain_progression

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -14,17 +14,17 @@ def progression_game(name):
         for inter in range(NUM_1, NUM_2, N):
             numbers.append(inter)
         index = numbers.index(numbers[random.randint(0, 5)])
+        correct_answer = numbers[index]
         numbers[index] = '..'
-        string = ' '.join(map(str, numbers))
+        string = ' '.join(map(str, numbers[0:10]))
         print(f'Question: {string}')
-
         answer = input('Your answer: ')
-        if numbers.index(answer) == numbers.index(index):
+        if  int(answer) == int(correct_answer):
             print('Correct!')
             i += 1
         else:
             print(f"'{answer}' is wrong answer ;(.")
-            print(f"'Correct answer was '{index}'.")
+            print(f"'Correct answer was '{correct_answer}'.")
             break
 
     print(congrats(name, i))
