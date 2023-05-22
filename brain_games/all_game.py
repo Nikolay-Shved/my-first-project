fe96139161b139409040cebfac607d9ca80db5b4
Nikolay Shved <viktor.vaskes44@gmail.com>
2023-05-22 17:19:58 +0800

    a

--- brain_games/all_game.py
+++ brain_games/all_game.py
@@ -1,5 +1,18 @@
 import prompt
 
+def greet():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('What is the result of the expression?')
+    return name
+
+
+def congrats(name, i):
+    if i == 1:
+        return f'Congratulations, {name}!'
+
+
 
 def game_loop(condition, game_body):
     print('Welcome to the Brain Games!')
