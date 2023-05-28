d75a232df5f0bb8b70b2871f3413086fe6a1e92d
Nikolay Shved <viktor.vaskes44@gmail.com>
2023-05-28 17:35:43 +0800

    modified brain_even

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -6,12 +6,13 @@ GAME_QUESTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def calculate(first_number):
     if first_number % 2 == 0:
-        return 
+        return True
 
 
 def get_question_and_answer():
     first_number = random.randint(START_SEQUENCE, END_SEQUENCE)
     correct_answer = calculate(first_number)
+    print(correct_answer)
     if correct_answer == True:
         correct_answer = 'yes'
     else:
